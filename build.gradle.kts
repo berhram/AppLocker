9f0edf5eba7a3227d969b9fa3fa3f7ae0877db97
velvet <berhram@protonmail.com>
2022-05-18 11:57:42 +0300

    build: bump gradle version 7.2 -> 7.4.2, gradle android version 7.1.3 -> 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.2.0")
     }
 }
 
 plugins {
-    id("com.android.application") version "7.1.3" apply false
+    id("com.android.application") version "7.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("org.jetbrains.kotlin.kapt") version "1.6.10" apply false
 }
