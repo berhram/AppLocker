26996135fc171d3ec608cb06f6da1fd8fa433f97
velvet <berhram@protonmail.com>
2022-05-21 17:11:15 +0300

    feat: gradle version catalog implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,50 +43,23 @@ android {
 }
 
 dependencies {
-    //Core
-    val coroutinesVersion = "1.6.1"
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-    //Orbit
-    val orbitVersion = "4.3.2"
-    implementation("org.orbit-mvi:orbit-core:$orbitVersion")
-    implementation("org.orbit-mvi:orbit-viewmodel:$orbitVersion")
-    //Room
-    val roomVersion = "2.4.2"
-    implementation("androidx.room:room-ktx:$roomVersion")
-    implementation("androidx.room:room-runtime:$roomVersion")
-    kapt("androidx.room:room-compiler:$roomVersion")
-    testImplementation("androidx.room:room-testing:$roomVersion")
-    //Compose
-    val composeVersion = "1.1.1"
-    val activityVersion = "1.4.0"
-    implementation("androidx.activity:activity-ktx:$activityVersion")
-    implementation("androidx.activity:activity-compose:$activityVersion")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-util:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.foundation:foundation:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.material:material-icons-core:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    //Koin
-    val koinVersion = "3.2.0"
-    implementation("io.insert-koin:koin-android:$koinVersion")
-    implementation("io.insert-koin:koin-androidx-compose:$koinVersion")
-    //Accompanist
-    val accompanistVersion = "0.23.1"
-    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-pager-indicators:$accompanistVersion")
-    //Testing
-    testImplementation("org.orbit-mvi:orbit-test:4.3.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.orbit)
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+    testImplementation(libs.room.testing)
+    implementation(libs.bundles.activity)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.accompanist)
+    testImplementation(libs.orbit.test)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junit.android)
+    androidTestImplementation(libs.ui.test.junit4)
 }
 
 kapt {
